6302e112030ff6779488ebcb0809cdfd8a9a6fd2
Cody Brunner <rockchalkwushock@icloud.com>
2017-08-17 18:43:09 -0500

    refactor(utils): Updated permissionsCheck conditional :alien:

--- server/utils/permissionsCheck.js
+++ server/utils/permissionsCheck.js
@@ -12,26 +12,8 @@ const { Campground, Comment } = require('../models')
  * will return false.
  */
 
-/**
- * FIXME
- * REVIEW
- *
- * Currently checking:
- *
- * `Object.keys(params).length`
- *
- * is a solid piece of code...however if someone maliciously
- * inserts into the url any number of params the else block
- * will run.
- *
- * 1) I could make a method that will filter the params object
- * only allowing 'id' &/or 'comment_id' to be passed through
- * for comparison checks
- * 2) I could just use `params.hasOwnProperty('prop')`.
- */
-
 module.exports = async ({ params, user }, res, next) => {
-  if (Object.keys(params).length === 1) {
+  if (params.id && !params.comment_id) {
     // Campground Permission Check
     try {
       const { author } = await Campground.findById(params.id)
